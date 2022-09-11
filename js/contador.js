8959e3705f7762c52adc171d31cd6e3818f21e27
eclipseCJP <cris.tecwordd@gmail.com>
2022-09-10 21:44:09 -0400

    projeto basico contador adicionado

---
+++ js/contador.js
@@ -0,0 +1,12 @@
+let menos=document.getElementById("menos");
+let mais=document.getElementById("mais");
+let numero=document.getElementById("numero");
+
+function incrementar(){
+    let num;
+     num=numero.value++;
+}
+function decrementar(){
+    let num
+    num=numero.value--;
+}
