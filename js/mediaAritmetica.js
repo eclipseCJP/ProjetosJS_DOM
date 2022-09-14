8c26e50d36d836cd2137952e263f361523c772af
eclipseCJP <cris.tecwordd@gmail.com>
2022-09-14 17:28:42 -0400

    versao 1 do switch case

---
+++ js/mediaAritmetica.js
@@ -0,0 +1,12 @@
+let nota1=document.getElementById("valor1");
+
+function calcular(){
+    let numero=parseInt(nota1.value);
+    console.log(numero);
+}
+
+//funcao para calcular a media aritmetica
+function media(numero){
+
+}
+
