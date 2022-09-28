c2e4132bb715ccfd117068bf00ca8cd361269721
eclipseCJP <cris.tecwordd@gmail.com>
2022-09-28 18:52:30 -0400

    manipulacao de array com metodo splice() e unshift()

--- js/arrayUni.js
+++ js/arrayUni.js
@@ -1,16 +1,50 @@
 let item=document.getElementById("item");
 let display=document.getElementById("display");
+let posicao=document.getElementById("item2");
 let list=[];
 
-function mostrarItem(){
+
+
+function ultimaPosition(){
     let itens=item.value;
     list.push(itens);
-    display.innerHTML=`<h3>${list}</h3>`;
+    display.innerHTML=`<li>[${list}]</li>`;
+}
+
+
+function primeiraPosition(){
+    let itens=item.value;
+    list.unshift(itens);
+    display.innerHTML=`<li>[${list}]</li>`;
 
 }
+
+
+function escolherPosition(){
+    let position=parseInt(posicao.value);
+    let itens=item.value;
+   list.splice(position,0,itens);
+   display.innerHTML=`<li>[${list}]</li>`;
+   console.log(position)
+
+}
+
+
 
 //1-METODO PUSH()
 /**
  * ALEM DE ADICIONAR O NOVO ITEM NA ULTIMA POSICAO
  * RETORNA O NOVO TAMANHO DO ARRAY
  */
+
+//2-METODO UNSHIFT()
+/**
+ * ADICIONA UM ELEMENTO NA PRIMEIRA POSICAO DO VETOR
+ **/
+
+//3-METODO SPLICE()
+/**
+ * O METODO PERMITE ALEM DE ESCOLHER A POSICAO ONDE SERÁ ADICIONADO 
+ * O NOVO ITEM TAMBEM PERMITE EXCLUIR.
+ * (POSICAO PARA ADICIONAR,POSICAO PARA EXCLUIR,ITEM A SER ADICIONADO)
+ **/
